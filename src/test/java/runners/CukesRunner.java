e57dd9b10accd589212b8e73cecd9fb87504fb35
ramazanyildirim1 <r.y3832@gmail.com>
2022-08-20 00:50:24 +0100

    cukes runner  has done

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features ="src/test/resources/features" ,
+        glue = "stepdefinitions" ,
+        dryRun = false,
+        tags = "@Test1",
+        publish= false
+)
+
+public class CukesRunner {
+
+}
