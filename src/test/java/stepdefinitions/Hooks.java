5210f30ba4dfc4c7b424e2a1aacbc43e06c1bc1a
ramazanyildirim1 <r.y3832@gmail.com>
2022-08-20 00:53:22 +0100

    Hook class setup

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,32 @@
+package stepdefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class Hooks {
+
+    @Before
+    public void setupScenarioForLogins(){
+        Driver.getDriver().get(ConfigurationReader.get("url.premierInn"));
+    }
+
+    @After
+    public void teardownScenario(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+        Driver.closeDriver();
+
+    }
+
+
+}
